f038c5041546c6d2abd72561194501fb72e9738a
wahhe <yuanhe326@ed.amdsb.ca>
2016-09-26 13:36:23 -0400

    Add project files.

---
+++ first Summative/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace first_Summative
+{
+    partial class TAURUS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TAURUS));
+            this.startbutton1 = new System.Windows.Forms.Button();
+            this.taurusTitleLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startbutton1
+            // 
+            this.startbutton1.Location = new System.Drawing.Point(321, 392);
+            this.startbutton1.Name = "startbutton1";
+            this.startbutton1.Size = new System.Drawing.Size(75, 23);
+            this.startbutton1.TabIndex = 0;
+            this.startbutton1.Text = "Start";
+            this.startbutton1.UseVisualStyleBackColor = true;
+            this.startbutton1.UseWaitCursor = true;
+            this.startbutton1.Click += new System.EventHandler(this.startbutton1_Click);
+            // 
+            // taurusTitleLabel
+            // 
+            this.taurusTitleLabel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.taurusTitleLabel.Font = new System.Drawing.Font("Trajan Pro", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taurusTitleLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.taurusTitleLabel.Location = new System.Drawing.Point(189, 216);
+            this.taurusTitleLabel.Name = "taurusTitleLabel";
+            this.taurusTitleLabel.Size = new System.Drawing.Size(369, 58);
+            this.taurusTitleLabel.TabIndex = 1;
+            this.taurusTitleLabel.Text = "THE TAURUS";
+            this.taurusTitleLabel.UseWaitCursor = true;
+            // 
+            // TAURUS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(639, 536);
+            this.Controls.Add(this.taurusTitleLabel);
+            this.Controls.Add(this.startbutton1);
+            this.Name = "TAURUS";
+            this.Text = "TAURUS";
+            this.UseWaitCursor = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startbutton1;
+        protected internal System.Windows.Forms.Label taurusTitleLabel;
+    }
+}
+
---
+++ first Summative/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace first_Summative
+{
+    public partial class TAURUS : Form
+    {
+        public TAURUS()
+        {
+            InitializeComponent();
+        }
+
+        private void startbutton1_Click(object sender, EventArgs e)
+        {
+            startbutton1.Hide();
+            taurusTitleLabel.Hide();
+            this.BackgroundImage = null;
+           
+            Refresh();
+
+            Graphics formGraphics = this.CreateGraphics();
+            Pen drawPen = new Pen(Color.Gold, 1);
+            SolidBrush drawBrush = new SolidBrush(Color.Gold);
+
+            formGraphics.Clear(Color.Black);
+
+            //draw spots
+            Thread.Sleep(500);
+            formGraphics.FillEllipse(drawBrush, 132, 40, 10, 10);
+            Thread.Sleep(500);
+            formGraphics.FillEllipse(drawBrush, 232, 70, 10, 10);
+            Thread.Sleep(500);
+            formGraphics.FillEllipse(drawBrush, 276, 98, 10, 10);
+            Thread.Sleep(500);
+            formGraphics.FillEllipse(drawBrush, 300, 115, 10, 10);
+            Thread.Sleep(500);
+            formGraphics.FillEllipse(drawBrush, 312, 122, 10, 10);
+            Thread.Sleep(500);
+            formGraphics.FillEllipse(drawBrush, 377, 40, 10, 10);
+            Thread.Sleep(500);
+            formGraphics.FillEllipse(drawBrush, 320, 135, 10, 10);
+            Thread.Sleep(500);
+            formGraphics.FillEllipse(drawBrush, 372, 168, 10, 10);
+            Thread.Sleep(500);
+            formGraphics.FillEllipse(drawBrush, 448, 172, 10, 10);
+            Thread.Sleep(500);
+            formGraphics.FillEllipse(drawBrush, 437, 162, 10, 10);
+            Thread.Sleep(500);
+            formGraphics.FillEllipse(drawBrush, 294, 138, 10, 10);
+            Thread.Sleep(500);
+            formGraphics.FillEllipse(drawBrush, 270, 132, 10, 10);
+            Thread.Sleep(500);
+            formGraphics.FillEllipse(drawBrush, 312, 122, 10, 10);
+            Thread.Sleep(500);
+            formGraphics.FillEllipse(drawBrush, 128, 129, 10, 10);
+            
+            //draw lines
+            Thread.Sleep(500);
+            formGraphics.DrawLine(drawPen, 139, 46, 232, 73);
+            formGraphics.DrawLine(drawPen, 232, 73, 276, 100);
+            formGraphics.DrawLine(drawPen, 276, 100, 300, 118);
+            formGraphics.DrawLine(drawPen, 300, 118, 312, 125);
+            formGraphics.DrawLine(drawPen, 312, 125, 377, 43);
+
+
+
+
+
+
+        }
+    }
+}
