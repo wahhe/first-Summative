bcf3589b2024e7ca8a6cb88f6fa1601100e09f02
wahhe <yuanhe326@ed.amdsb.ca>
2016-09-28 13:36:18 -0400

    3rd commit

--- first Summative/Form1.cs
+++ first Summative/Form1.cs
@@ -8,7 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
+using System.Media;
 
+//creat by WAH HE, 20160926, first summative
 namespace first_Summative
 {
     public partial class TAURUS : Form
@@ -25,74 +27,101 @@ namespace first_Summative
             this.BackgroundImage = null;
            
             Refresh();
-
+            
+            //graphics objects
             Graphics formGraphics = this.CreateGraphics();
-            Pen drawPen = new Pen(Color.Gold, 1);
-            SolidBrush drawBrush = new SolidBrush(Color.Gold);
-            //create a font
+            Pen linePen = new Pen(Color.Lime, 2);
+            SolidBrush goldBrush = new SolidBrush(Color.Gold);
+            Font tittleFont = new Font("Trajan Pro", 22, FontStyle.Bold);
+            SolidBrush yellowBrush = new SolidBrush(Color.LightGoldenrodYellow);
+            Font bodyFont = new Font("Georage", 16, FontStyle.Bold);
+            
 
             formGraphics.Clear(Color.Black);
 
             //draw spots
             Thread.Sleep(400);
-            formGraphics.FillEllipse(drawBrush, 132, 40, 10, 10);
+            formGraphics.FillEllipse(goldBrush, 132, 40, 10, 10);
             Thread.Sleep(400);
-            formGraphics.FillEllipse(drawBrush, 232, 70, 10, 10);
+            formGraphics.FillEllipse(goldBrush, 232, 70, 10, 10);
             Thread.Sleep(400);
-            formGraphics.FillEllipse(drawBrush, 276, 98, 10, 10);
+            formGraphics.FillEllipse(goldBrush, 276, 98, 10, 10);
             Thread.Sleep(400);
-            formGraphics.FillEllipse(drawBrush, 300, 115, 10, 10);
+            formGraphics.FillEllipse(goldBrush, 300, 115, 10, 10);
             Thread.Sleep(400);
-            formGraphics.FillEllipse(drawBrush, 312, 122, 10, 10);
+            formGraphics.FillEllipse(goldBrush, 312, 122, 10, 10);
             Thread.Sleep(400);
-            formGraphics.FillEllipse(drawBrush, 377, 40, 10, 10);
+            formGraphics.FillEllipse(goldBrush, 377, 40, 10, 10);
             Thread.Sleep(400);
-            formGraphics.FillEllipse(drawBrush, 320, 135, 10, 10);
+            formGraphics.FillEllipse(goldBrush, 320, 135, 10, 10);
             Thread.Sleep(400);
-            formGraphics.FillEllipse(drawBrush, 372, 168, 10, 10);
+            formGraphics.FillEllipse(goldBrush, 372, 168, 10, 10);
             Thread.Sleep(400);
-            formGraphics.FillEllipse(drawBrush, 448, 172, 10, 10);
+            formGraphics.FillEllipse(goldBrush, 448, 172, 10, 10);
             Thread.Sleep(400);
-            formGraphics.FillEllipse(drawBrush, 437, 162, 10, 10);
+            formGraphics.FillEllipse(goldBrush, 437, 162, 10, 10);
             Thread.Sleep(400);
-            formGraphics.FillEllipse(drawBrush, 294, 138, 10, 10);
+            formGraphics.FillEllipse(goldBrush, 294, 138, 10, 10);
             Thread.Sleep(400);
-            formGraphics.FillEllipse(drawBrush, 270, 132, 10, 10);
+            formGraphics.FillEllipse(goldBrush, 270, 132, 10, 10);
             Thread.Sleep(400);
-            formGraphics.FillEllipse(drawBrush, 312, 122, 10, 10);
+            formGraphics.FillEllipse(goldBrush, 312, 122, 10, 10);
             Thread.Sleep(400);
-            formGraphics.FillEllipse(drawBrush, 128, 129, 10, 10);
+            formGraphics.FillEllipse(goldBrush, 128, 129, 10, 10);
             
             //draw lines
             Thread.Sleep(500);
-            formGraphics.DrawLine(drawPen, 139, 46, 232, 73);
+            formGraphics.DrawLine(linePen, 139, 46, 232, 73);
             Thread.Sleep(300);
-            formGraphics.DrawLine(drawPen, 232, 73, 276, 100);
+            formGraphics.DrawLine(linePen, 232, 73, 276, 100);
             Thread.Sleep(300);
-            formGraphics.DrawLine(drawPen, 276, 100, 300, 118);
+            formGraphics.DrawLine(linePen, 276, 100, 300, 118);
             Thread.Sleep(300);
-            formGraphics.DrawLine(drawPen, 300, 118, 312, 125);
+            formGraphics.DrawLine(linePen, 300, 118, 312, 125);
             Thread.Sleep(300);
-            formGraphics.DrawLine(drawPen, 315, 130, 383, 43);
+            formGraphics.DrawLine(linePen, 315, 130, 383, 43);
             Thread.Sleep(300);
-            formGraphics.DrawLine(drawPen, 318, 130, 323, 140);
+            formGraphics.DrawLine(linePen, 318, 130, 323, 140);
             Thread.Sleep(300);
-            formGraphics.DrawLine(drawPen, 320, 138, 372, 170);
+            formGraphics.DrawLine(linePen, 320, 138, 372, 170);
             Thread.Sleep(300);
-            formGraphics.DrawLine(drawPen, 372, 173, 450, 177);
+            formGraphics.DrawLine(linePen, 372, 173, 450, 177);
             Thread.Sleep(300);
-            formGraphics.DrawLine(drawPen, 450, 177, 440, 165);
+            formGraphics.DrawLine(linePen, 450, 177, 440, 165);
             Thread.Sleep(300);
-            formGraphics.DrawLine(drawPen, 320, 140, 300, 143);
+            formGraphics.DrawLine(linePen, 320, 140, 300, 143);
             Thread.Sleep(300);
-            formGraphics.DrawLine(drawPen, 300, 143, 270, 135);
+            formGraphics.DrawLine(linePen, 300, 143, 270, 135);
             Thread.Sleep(300);
-            formGraphics.DrawLine(drawPen, 270, 136, 128, 133);
+            formGraphics.DrawLine(linePen, 270, 136, 128, 133);
 
-            //write text
-            Font drawFont = new Font("Georage", 16, FontStyle.Bold);
-            formGraphics.DrawString("TAURUS.", drawFont, drawBrush, 20, 20);
-            formGraphics.DrawString(" ", drawFont, drawBrush, 20, 40);
+            //write text part
+            //add title to the descripitions
+
+            formGraphics.DrawString("TAURUS", tittleFont, yellowBrush,250, 220);
+            
+            //add subtittle text
+            Thread.Sleep(100);
+            formGraphics.DrawString("Governing element: Earth", bodyFont, goldBrush, 50, 260);
+            Thread.Sleep(100);
+            formGraphics.DrawString("Ruling planet: Venus", bodyFont, goldBrush, 50, 290);
+            Thread.Sleep(100);
+            formGraphics.DrawString("Quality: Fixed", bodyFont, goldBrush, 50, 320);
+            Thread.Sleep(100);
+            formGraphics.DrawString("Polarity: Negative", bodyFont, goldBrush, 50, 350);
+            Thread.Sleep(100);
+            formGraphics.DrawString("Period of Birth: (New Date)May 13 – June 21", bodyFont, goldBrush, 50, 380);
+            Thread.Sleep(100);
+            formGraphics.DrawString("Compatible Signs: Cancer, Virgo, Scorpio, Pisces", bodyFont, goldBrush, 50, 410);
+            Thread.Sleep(100);
+            formGraphics.DrawString("Incompatible Signs: Gemini, Leo, Libra, ", bodyFont, goldBrush, 50, 440);
+            Thread.Sleep(100);
+            formGraphics.DrawString("Variably compatible Signs: Scorpio", bodyFont, goldBrush, 50, 470);
+
+            
+
+
+
 
 
 
